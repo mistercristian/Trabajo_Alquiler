0d065628ccc379149d10796427a7898fe319dd0e
mistercristian <cristianguzbo@gmail.com>
2024-05-20 22:17:26 -0500

    Page User

---
+++ Pages/Users/Edit.cshtml.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Trabajo_Alquiler.Data;
+using Trabajo_Alquiler.Models;
+
+namespace Trabajo_Alquiler.Pages.Users
+{
+    public class EditModel : PageModel
+    {
+        private readonly AlquilerContext _context;
+
+        public EditModel(AlquilerContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public User User { get; set; } = default!;
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            User = user;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(User).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(User.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool UserExists(int id)
+        {
+            return (_context.Users?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
+
