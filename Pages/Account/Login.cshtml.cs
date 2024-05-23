6043ee0600c0498192d276d3ee4ddcd187ab6d8b
Kevin-SantiagoC <santiagocastano47@gmail.com>
2024-05-22 22:54:11 -0500

    Crear metodo OnPost

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,12 +1,22 @@
+using Trabajo_Alquiler.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace Trabajo_Alquiler.Pages.Account
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+
+        public string User {get; set; }
         public void OnGet()
         {
+        }
+
+        public void OnPost() 
+        {
+            Console.WriteLine("User: " + User.Email + " Password: " + User.Password);
         }
     }
 }
