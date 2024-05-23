2e7889520b43947b4054c60f66377470e782ac0b
Kevin-SantiagoC <santiagocastano47@gmail.com>
2024-05-22 23:08:29 -0500

    Verificacion de credenciales

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -14,8 +14,19 @@ namespace Trabajo_Alquiler.Pages.Account
         {
         }
 
-        public void OnPost() {
-            Console.WriteLine("User: " + User.Email + " Password: " + User.Password);
+        public async Task<IActionResult> OnPostAsync() 
+        {
+        if(!ModelState.IsValid) return Page();
+
+            if (User.Email == "Santiago@gmail.com" && User.Password == "12345") 
+            {
+                return RedirectToPage("/index");
+            }
+            return Page();
         }
+
+       /* public void OnPost() {
+            Console.WriteLine("User: " + User.Email + " Password: " + User.Password);*/
+        
     }
 }
