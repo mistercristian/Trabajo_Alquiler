9bd7763f7f1f6e7089d68e3880db8cfb5588b521
Kevin-SantiagoC <santiagocastano47@gmail.com>
2024-05-22 23:31:55 -0500

    Generar Cookie

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,6 +1,8 @@
 using Trabajo_Alquiler.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 
 
 namespace Trabajo_Alquiler.Pages.Account
@@ -20,6 +22,17 @@ namespace Trabajo_Alquiler.Pages.Account
 
             if (User.Email == "Santiago@gmail.com" && User.Password == "12345") 
             {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email,User.Email)
+                };
+                 var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
                 return RedirectToPage("/index");
             }
             return Page();
